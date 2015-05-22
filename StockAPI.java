36a371d0fe56a78dfaf0a12d12b151caa4c64067
roddajohn <johnrod.john@gmail.com>
2015-05-22 11:45:40 -0400

    First hack at it

---
+++ StockAPI.java
@@ -0,0 +1,12 @@
+public class StockAPI {
+    public final String BASEQUOTEURL = "http://download.finance.yahoo.com/d/quotes.csv?s=";
+    public final String NAMEPROPERTY = "n";
+    public final String SYMBOLPROPERTY = "s";
+    public final String LATESTVALUEPROPERTY = "l1";
+    public final String OPENLASTTRADINGDAYPROPERTY = "o";
+    public final String CLOSELASTTRADINGDAYPROPERTY = "p";
+
+    public Stock getStock(String symbol) {
+
+    }
+}
