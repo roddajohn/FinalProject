7aa1972ff77794abf83c810242e2fa7d78eaf76f
roddajohn <johnrod.john@gmail.com>
2015-05-26 21:13:50 -0400

    Updated

--- Client/Player.java
+++ Client/Player.java
@@ -1,13 +1,22 @@
 import java.util.*;
 
 public class Player {
-    private ArrayList<ArrayList<Stock>> stocks;
+    private ArrayList<String> stockNames;
+    private ArrayList<Integer> stockAmts;
     private double money;
     private String username;
+    private PlayerCommunication p;
 
     public Player(String u) {
 	username = u;
-	stocks = new ArrayList<ArrayList<Stock>>();
+	stocks = new ArrayList<String>();
+	stockAmts = new ArrayList<Integer>();
 	money = 100000.0;
+	p = new PlayerCommunication();
     }
+
+    public void buyStock(String s, int amt) {
+	
+    }
+
 }
