257481ee560798ad2354a60b461c114125e309ee
Crispin226 <benlanier24@gmail.com>
2015-06-08 06:29:49 -0500

    U

---
+++ Server/Driver2.java
@@ -0,0 +1,51 @@
+public class Driver2 {
+    
+    public static void main(String [ ] args){
+        
+    Player P = new Player("Benjamin", "25", 3254.0, { Google, Apple, Exxon} , {30,40, 50} , false);
+    TradingAlgorithm TA = new TradingAlgorithm();
+        
+        
+         ExpectedDifference12 += (currentValue - currentValue1) * WcurrentValue;
+            ExpectedDifference12 += (closeLastDay - closeLastDay1) * WcloseLastDay;
+            ExpectedDifference12 += (openLastDay - openLastDay1) * WopenLastDay;
+            ExpectedDifference12 += (BID - BID1) * WBID;
+            ExpectedDifference12 += (TargetEstimate - TargetEstimate1) * WTargetEstimate;
+            ExpectedDifference12 += (Beta - Beta1) * WBeta;
+            ExpectedDifference12 += (DaysRange - DaysRange1) * WDaysRange;
+            ExpectedDifference12 += (Week52Volume - Week52Volume1) * WWeek52Volume;
+            ExpectedDifference12 += (Volume - Volume1) * WVolume;
+            ExpectedDifference12 += (AvgVolume - AvgVolume1) * WAvgVolume;
+            ExpectedDifference12 += (MarketCapitalization - MarketCapitalization1) * WMarketCapitalization;
+            ExpectedDifference12 += (EPS - EPS1) * WEPS;
+            ExpectedDifference12 += (DividendsandYield - DividendsandYield1) * WDividendsandYield;
+            ExpectedDifference12 += (P2SR - P2SR1) * WP2SR;                               
+            ExpectedDifference12 += (FP2E - FP2E1) * WFP2E;
+            ExpectedDifference12 += (AEPS - AEPS1) * WAEPS;
+            ExpectedDifference12 += ( QEPS -  QEPS1) * WQEPS;
+            ExpectedDifference12 += (MeanReccomendations - MeanReccomendations1) * WMeanReccomendations;
+            ExpectedDifference12 += (PEGRatio - PEGRatio1) * WPEGRatio;
+            ExpectedDifference12 += (currentValue - currentValue1) * WcurrentValue;  
+        
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
